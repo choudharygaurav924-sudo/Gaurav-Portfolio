ab5c72eedc654f1546bd08ca27f3839c26cb15d9
Gaurav <choudharygaurav924@gmail.com>
2026-09-24 14:12:55 +0530

    Implement Stage 1 cinematic editorial opening sequence

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,14 +2,7 @@
 
 import type { ReactNode } from "react";
 import { SmoothScrollProvider } from "./SmoothScrollProvider";
-import { Skiper10 } from "./ui/skiper10";
-import { BRAND } from "@/lib/data";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <SmoothScrollProvider>
-      <Skiper10 wordmark={BRAND.name} />
-      {children}
-    </SmoothScrollProvider>
-  );
+  return <SmoothScrollProvider>{children}</SmoothScrollProvider>;
 }
